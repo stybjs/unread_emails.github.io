95b1c2ed8ea1a19a8a21b4700acaf8a03dabfa0a
Tian <79352779+stybjs@users.noreply.github.com>
2025-12-10 22:44:49 -0500

    added more messages

---
+++ script.js
@@ -0,0 +1,410 @@
+  const emailData = {
+  1: {
+    sender: "Tessa Rowe",
+    email: "tessa.rowe@wellmind-consulting.com",
+    subject: "Follow-up Appointment",
+    body: `
+            Hi Riley, 
+	<br>
+		<br>
+            I hope this message finds you well. I am concerned about the last several sessions you missed and I would
+            like to remind you that you need to attend your sessions at this month.<br><br>
+
+            If you keep doing this, you are damaging your mental health progress and I understand you have issues with your romantic relationship.
+            Please make an appointment to see me as soon as possible so we can discuss your situation and find ways to help you cope.<br><br>
+            
+            Looking forward to hearing from you.<br><br>
+            
+            Best regards,<br>
+            Dr. Tessa Rowe<br>
+            WellMind Consulting
+      `,
+  },
+  2: {
+    sender: "Rowan Pike",
+    email: "rowan.pike@arcturus-security.com",
+    subject: "Quick Check-In",
+    body: `
+            Hi, <br>br>
+            I noticed a small discrepancy in last week’s badge access logs and wanted to double-check. <br><br> 
+            Your ID pinged near the West Maintenance Wing at 20:45 on Thursday. <br><br> 
+            Probably just a scanner glitch — those readers misfire all the time. <br>br>
+            When you get a moment, can you confirm whether you were anywhere near that hallway? <br><br> No rush.
+
+            Thanks! <br><br>
+            - Rowan Pike<br>
+      `,
+  },
+  3: {
+    sender: "Ria Callow",
+    email: "ria.callow@arcturus-corp.com",
+    subject: "Lunch Tomorrow?",
+    body: `
+            Hey! Haven’t seen you around much this week, is everything okay? 
+            I was going to ask you if you wanted to grab lunch tomorrow like we planned. 
+            If you are busy, no worries! You can always find me in my favorite break spot!
+            Text me if that’s easier.
+            -Ria
+      `,
+  },
+  4: {
+    sender: "Victor Haines",
+    email: "v.haines@arcturus-corp.com",
+    subject: "Password Rotation Reminder",
+    body: `
+            Hello,
+
+            This is a friendly reminder that your account password has not been 
+            updated in over 90 days. For security compliance, please complete your 
+            password rotation by the end of the week.
+
+            If you’ve already updated it, you may disregard this message.
+
+            Best Regards,
+            Victor Haines
+            Compliance Officer
+      `,
+  },
+  5: {
+    sender: "Camila Jun",
+    email: "camila.juno@arcturus-secops.com",
+    subject: "System Patch Reminder (Minor Issue)",
+    body: `
+            Hi,
+
+            I noticed your workstation hasn't installed last week's security patch yet. 
+            It's not a big deal — a few machines missed the update because of a server 
+            hiccup — but I wanted to give you a heads up.
+
+            When you get the chance, restart your machine so the patch can finish.
+
+            Thanks,
+            Camila
+            IT Security Analyst
+      `,
+  },
+  6: {
+    sender: "Nighthawk",
+    email: "no-reply@nighthawk.system",
+    subject: ".. / -.- -. --- .-- / -.-- --- ..-",
+    body: `
+        .. / -.- -. --- .-- / .-- .... .- - / -.-- --- ..- / .- .-. . / .-.. --- --- -.- .. -. --. 
+        / ..-. --- .-. .-.-.- / .- .-. -.-. - ..- .-. ..- ... .-.-.- / -.-- --- ..- / .-- .- -. - 
+        / .- / - .-. ..- - .... --..-- / -.. --- / -.-- --- ..- ..--.. / -.. --- .-- -. .-.. --- .- -.. 
+        / .- / ..-. .. .-.. . / .- -. -.. / --- .--. . -. / .. - .-.-.-
+      `,
+  },
+  7: {
+    sender: "Adrian Cross",
+    email: "across1130@tmail.com",
+    subject: "We Need to Talk",
+    body: `
+    You keep saying you're busy with work, even though you can request a leave. I do not understand why you keep avoiding me like this. If you keep doing this, I do not know what else I will do, like leave you here. 
+
+    I am trying to understand you, okay? Just a simple conversation is enough. 
+
+    I will be away for work. Text me if you are home. We can talk later when we have time.
+      `,
+  },
+  8: {
+    sender: "Tessa Rowe",
+    email: "tessa.rowe@wellmind-consulting.com",
+    subject: "Checking In — Noticed a Change",
+    body: `
+    Hi Riley,<br><br>
+
+    I wanted to follow up because you've missed several sessions in the past week, and I’m concerned. 
+    You also mentioned your need for medication. I am afraid not since I am not a psychiatrist. <br>br>
+
+    Let me know how you're doing. I'm really worried about you. You can always ask for my referral to obtain medication. 
+    Please remember that your discussion is significant.
+
+    Please let me know how you're doing and I am really worrying about you. 
+    You have not responded to emails, even my calls. You can always contact my office, 
+    they can assist you to schedule your session. 
+    
+    Please at least let me know you sound safe, okay?
+
+    Take care,<br>
+    Dr. Tessa Rowe
+  `,
+  },
+  9: {
+    sender: "Rowan Pike",
+    email: "rowan.pike@arcturus-security.com",
+    subject: "Your Replacement Card",
+    body: `
+    Hello Riley,
+    
+    Your request for a replacement employee ID card has been processed. 
+    You can pick it up at the Security Office on Level 2 anytime during weekday hours.<br><br>
+
+    The request was submitted through your account early this morning.
+    Here’s a reminder to keep your new card secured at all times and report any unusual activity immediately.<br><br>
+    Please note that your request for a replacement employee ID card has been recorded. Your limit of requests for this is up to 2 per year.
+    
+    If you have any questions or have concerns with using your employee card, please do not hesitate to contact our Arcturus Security team.
+    
+    Best Regards,
+    Rowan
+  `,
+  },
+  10: {
+    sender: "Ria Callow",
+    email: "ria.callow@arcturus-corp.com",
+    subject: "Team invitation?",
+    body: `
+      It’s crazy that somebody in our team was accused of leaking confidential information, even when they are innocent. 
+      I guess some people can be innocent while hiding evil intentions. Our company treats us pretty well, so why do they do that?
+
+      Anyways, do you remember the senior guy we saw? They will join our team night dinner, if you are interested. Let me know! 
+
+      I know you have marriage issues. You shouldn’t bring your personal issues into our workplace.  
+      Why don’t you divorce them? I mean they are pretty bad. They should be understandable about your work. 
+      Wouldn't it be better if you told them the truth?
+      
+      Ah, I gotta work on my assignment since it is overdue!!! Message me later when you get off work! 
+      
+      - Ria
+    `,
+  },
+  11: {
+    sender: "Victor Haines",
+    email: "v.haines@arcturus-corp.com",
+    subject: "Routine Internal Audit",
+    body: `
+      Riley,
+      Your account had irregularities that I noticed during my compliance audit.
+      They may be nothing more than errors, but I will need to examine them more closely.
+      
+      You may be contacted by one of our team, if we need more information from you regarding your account.
+      
+      Victor
+    `,
+  },
+  12: {
+    sender: "Camila Jun",
+    email: "camila.juno@arcturus-secops.com",
+    subject: "Login Account Compromised ",
+    body: `
+        Hello Riley,
+        There has been a breach of your account. Please immediately change your password. 
+        If you cannot update your password, please contact us right away to reset it.
+        Check any unusual changes in your account, including your company balance you received based on your performance review. 
+        
+        We, our information security department, cannot restore or return funds to your account if your balance or sensitive information is stolen; please be aware.
+        
+        - IT Security Analyst Camila Juno
+    `,
+  },
+  13: {
+    sender: "Nighthawk",
+    email: "no-reply@nighthawk.system",
+    subject: "69 115 99 97 112 101",
+    body: `
+        83 111 109 101 111 110 101 32 105 115 32 119 97 116 99 104 105 110 103 32 121 111 117 
+        46 32 73 32 99 97 110 32 104 101 108 112 32 121 111 117 44 32 111 110 108 121 105 
+        102 32 121 111 117 32 109 97 107 101 32 100 101 99 105 115 105 111 110 32 110 111 119 46
+      `,
+  },
+  14: {
+    sender: "Adrian Cross",
+    email: "across1130@tmail.com",
+    subject: "Help",
+    body: `
+        Strangers came to my door tonight and they just knocked and asked if you were at home. I just answered no because I do not know who they are. 
+        What’s going on, can you tell me? I am really worried because they were quite creepy. They were fully dressed in black and did not blink at all. 
+        I tried to tell them, “Please leave”. They just stayed there like that—they stood and looked like dolls or something like that. I mean 
+        I do not know what to do.
+        
+        Please come home ASAP! No excuse at all!
+      `,
+  },
+  15: {
+    sender: "Tessa Rowe",
+    email: "tessa.rowe@wellmind-consulting.com",
+    subject: "Past Due Bill",
+    body: `
+    Hi Riley,   
+    I want to let you know that your balance has passed its due date because your insurance claim is not processed. 
+    Please contact your insurance to resolve your billing. 
+    Meanwhile, you can access your billing portal to make a payment plan, which you can avoid late penalty fees for late payment.
+    If you have any questions about this, feel free to contact me via phone or email. 
+
+    Thank you,<br>
+    Dr. Tessa Rowe<br>
+    WellMind Consulting
+  `,
+  },
+  16: {
+    sender: "Tessa Rowe",
+    email: "tessa.rowe@wellmind-consulting.com",
+    subject: "About What You Said",
+    body: `
+    Riley,<br><br>
+    I have been thinking about the topic you brought up during our last session. 
+    It was nothing serious; you were doing it right. 
+    But, I want you to be aware that if your information is shared with me, I am obliged to keep our discussion confidential. 
+    I think you should not give up, and of course you can always seek support from me. I am always here.
+
+    — Tessa
+  `
+  },
+  17: {
+    sender: "Rowan Pike",
+    email: "rowan.pike@arcturus-security.com",
+    subject: "Automated Alert Review",
+    body: `
+      Riley,
+      Your account for the usage of employee cards has been flagged by an automated detection system. 
+      I’ve cleared an alert, and it won’t escalate to compliance level. 
+      Your employee card will be put on a watchlist due to unusual activity. 
+      If you encounter any problems with this, there's no need to open a ticket. Just keep me informed if you require temporary access.
+      The issue has been resolved, and please refrain from bringing up any other issues in the future.
+      
+      Rowan
+      `,
+  },
+  18: {
+    sender: "Ria Callow",
+    email: "ria.callow@arcturus-corp.com",
+    subject: "Urgent: Read This",
+    body: `
+      Something strange happened. I saw someone keep checking on you every time you came in every morning. 
+      You didn’t do anything… right? I mean, you wouldn’t…, so I did not tell them anything when they asked me.
+      
+      But, they sent me a file and asked me to forward it to you. I did not open it, but I got the feeling that this is too suspicious.
+      Lately, something in our workplace is strange. I mean I can’t describe it at all. Especially our veteran employee….He kind of freaked me out. 
+      He kept telling us that we were not safe.
+      
+      Probably just gibberish. But, just be safe.
+      
+      - Ria
+      `,
+    attachment: "attachments/urgent_read_this.txt",
+    attachmentName: "urgent_read_this.txt",
+  },
+  19: {
+    sender: "Victor Haines",
+    email: "v.haines@arcturus-corp.com",
+    subject: "Compliance Review",
+    body: `
+         Riley,
+         I am obligated to let you know that the files you accessed last month are currently being examined internally. This notice does not imply misconduct. 
+         You may be contacted by one of us to request your statement on certain files.
+         It is prohibited to alter or remove any departmental files during this period, as any attempt to do so will be detected and handled immediately.
+         If you believe someone else may have used your credentials, you should notify us immediately.
+         
+         - Victor, Compliance Officer at Arcturus Corporation
+      `,
+  },
+  20: {
+    sender: "Camila Jun",
+    email: "camila.juno@arcturus-secops.com",
+    subject: "Account Suspended ",
+    body: `
+        Hello Riley,
+        Your account was flagged immediately after our automated system detected illegal activity on our servers. 
+        Your account is currently suspended due to a security policy you have violated. 
+        You will not provide a temporary account based on your case. 
+        Following the review conducted by Arcturus SecOps, we found that you have accessed restricted materials multiple times according to your credentials that showed recorded logs of your activity.
+        
+        We do not disclose the nature of your access restricted materials, as they are only accessed by higher levels. 
+        Please do not attempt to access, log in, reset the password, or anything related to the employee access system. 
+        Any unusual activity will be recorded and may be used against your case.
+        This message serves as a confirmation of your case currently under ongoing investigation.
+        
+        - IT Security Analyst Camila Juno
+  `,
+  },
+  21: {
+    sender: "Nighthawk",
+    email: "no-reply@nighthawk.system",
+    subject: "Tllapun Zwva",
+    body: `
+        Tlla tl ha fvby bubzbhs zwva. Kv uva alss hufivkf. Jvtl olyl ha 11 huk slhcl lclyfaopun vu fvby klzr. 
+        Fvb'ss npclu h uld jsvaolz huk zovlz dolu h ovtlslzz thu kyvw ulea av tlaly whyrpun. 
+        Kv uva svvr ihjr huk johunl fvby jsvaolz ha Ybsh Ybsh Jhml, dopjo pz ulea 6 isvjrz myvt fvby dvyrwshjl.
+      `,
+  },
+  22: {
+    sender: "Adrian Cross",
+    email: "across1130@tmail.com",
+    subject: "Why Did You Do It?!",
+    body: `
+        I will keep in short, since you are busy and have urgent tasks or whatever that is.    
+        You told me about your concern….that is too dangerous. You need to think about your life and mine too. 
+        
+        Did you not care about me at all? At least an ounce? No? Tell me.
+        I understand your morals, but that is not right. I mean they could do anything. Just like strangers came to our house. 
+        
+        I am sorry, I can’t stay here. God, they are here. 
+        
+        Bye
+      `,
+  },
+  23: {
+    sender: "Tessa Rowe",
+    email: "tessa.rowe@wellmind-consulting.com",
+    subject: "Please Stop",
+    body: `
+        Riley,<br><br>
+        
+        Do not send me any more documents. I should not have opened or seen the first one.
+        I deleted it immediately, but I can’t be involved in this.
+        
+        Please — whatever you’re trying to do, be careful.
+        
+        — T
+        `,
+  },
+  24: {
+    sender: "Tessa Rowe",
+    email: "tessa.rowe@wellmind-consulting.com",
+    subject: "Patient Notice",
+    body: `
+        Riley,<br><br>
+        I am sorry that I cannot promise you at this time. I am ending our sessions and destroying our confidential information. 
+        Please stop contacting me, I have my life and I do not want to be involved with your problems.
+        
+        If you persist in coming to my office, I have the right to evict you, beside you are not my guest anymore.
+        
+        — Dr. Rowe
+        `,
+  }
+};
+
+
+  const mailItems = document.querySelectorAll(".mail-list");
+  const readingPane = document.getElementById("readingPane");
+
+  mailItems.forEach(item => {
+    item.addEventListener("click", () => {
+      const prev = document.querySelector('.mail-list.active');
+      if (prev) {
+        prev.classList.remove('active');
+      }
+
+      item.classList.add("active");
+
+      const id = item.getAttribute("data-id");
+      const email = emailData[id];
+
+      readingPane.innerHTML = `
+      <div class="p-4">
+        <h4>${email.subject}</h4>
+        <p><strong>From:</strong> ${email.sender} &lt;${email.email}&gt;</p>
+        <hr>
+        <p>${email.body}</p>
+
+        ${email.attachment ? `
+          <hr>
+          <p><strong>Attachment:</strong></p>
+          <a href="${email.attachment}" download="${email.attachmentName}" class="btn btn-outline-primary">
+            <i class="mdi mdi-download"></i> Download File
+          </a>
+        ` : ""}
+      </div>
+    `;
+    });
+  });
